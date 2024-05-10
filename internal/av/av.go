128ceba26a105452e401f90f35baf3ba818c0e67
Jesper Lillie <jesper.lillie@gmail.com>
2024-05-10 17:41:47 +1000

    C code broken out into its own file instead of being a comment block

--- internal/av/av.go
+++ internal/av/av.go
@@ -9,31 +9,7 @@ package av
 #cgo CFLAGS: -Wall
 #cgo LDFLAGS: -lavformat -lavutil -lavcodec -lavfilter
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <inttypes.h>
-
-#include <libavformat/avformat.h>
-#include <libavcodec/avcodec.h>
-#include <libavutil/dict.h>
-#include <libavutil/pixdesc.h>
-#include <libavutil/opt.h>
-#include <libavfilter/buffersink.h>
-#include <libavfilter/buffersrc.h>
-
-int get_pix_fmt(enum AVPixelFormat* fmts, enum AVPixelFormat hope) {
-	for (int i = 0; fmts[i] != -1; i++) {
-	if (fmts[i] == hope) {
-			return i;
-		}
-     }
-	return -1;
-}
-
-AVStream *get_nth_stream(AVFormatContext *fmt_ctx, uint i) {
-	return fmt_ctx->streams[i];
-}
+#include "helpers.h"
 */
 import "C"
 
